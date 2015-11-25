40fb1137d21b45a26b33d0db7c17bafe5fc69353
Katarzyna Adrabinska <katarzyna.adr@gmail.com>
2015-11-25 19:27:01 +0100

    Simple Exchange calculator with web api

--- exchange.py
+++ exchange.py
@@ -1,3 +1,5 @@
+import json
+import requests
 
 class UnsupportedExchangeRateError(Exception):
     """Exception raised when there's not enough data to preform exchange"""
@@ -6,9 +8,39 @@ class UnsupportedExchangeRateError(Exception):
 
 class CurrencyExchanger(object):
     def __init__(self, exchange_rates=None, default_currency=None):
-        pass
+        self.exchange_rates = exchange_rates
+        self.default_currency = default_currency
 
     def exchange(self, value, input_currency, output_currency=None):
-        if value == 2:
-            return 8.48  # do you have good enough tests? ;)
-        return 4.24
+        output_currency = output_currency or self.default_currency
+
+        if input_currency == output_currency:
+            return value
+
+        if not output_currency:
+            raise UnsupportedExchangeRateError('Brak waluty wyjsciowej i domyslnej')
+
+        exchange_rate = self.exchange_rates.get(
+            (input_currency, output_currency)
+        )
+
+        if exchange_rate is None:
+            raise UnsupportedExchangeRateError('Brak takiego kursu')
+
+        if isinstance(exchange_rate, str) or isinstance(value, str):
+            raise UnsupportedExchangeRateError('przelicznik/ilosc nie jest liczba')
+
+        if value < 0:
+            raise UnsupportedExchangeRateError('wartosc jest ujemna')
+
+        return exchange_rate * value
+
+
+class OnlineRates(object):
+
+    def __init__(self, src=None):
+        self.src = src
+
+    def get_data(self):
+        rates = json.loads(requests.get(self.src))
+        return rates
--- test_exchange.py
+++ test_exchange.py
@@ -1,5 +1,6 @@
+import mock
 import pytest
-from exchange import CurrencyExchanger, UnsupportedExchangeRateError
+from exchange import CurrencyExchanger, UnsupportedExchangeRateError, OnlineRates
 
 
 # example exchange rates for testing
@@ -22,8 +23,16 @@ def test_pln_to_euro():
 
 
 def test_euro_to_pln_with_default_pln():
-    # TODO: implement
-    assert False
+    e = CurrencyExchanger(exchange_rates={('eur', 'pln'): 4.24},
+                          default_currency='pln')
+    assert e.exchange(1, 'eur') == 4.24
+    assert e.exchange(1, 'pln') == 1
+
+
+def test_lack_of_default_and_output_currency():
+    e = CurrencyExchanger(exchange_rates={('eur', 'pln'): 4.24})
+    with pytest.raises(UnsupportedExchangeRateError):
+        e.exchange(1, 'eur')
 
 
 def test_unsupported_exchange_rates():
@@ -34,13 +43,21 @@ def test_unsupported_exchange_rates():
 
 
 def test_wrong_value():
-    # TODO: implement
-    assert False
+    e = CurrencyExchanger(exchange_rates={('eur', 'pln'): 'asd'})
+    with pytest.raises(UnsupportedExchangeRateError):
+        e.exchange(1, 'eur', 'pln')
+
+    e = CurrencyExchanger(exchange_rates={('eur', 'pln'): 4.24})
+    with pytest.raises(UnsupportedExchangeRateError):
+        e.exchange('1', 'eur', 'pln')
+
+    with pytest.raises(UnsupportedExchangeRateError):
+        e.exchange(-1, 'eur', 'pln')
 
 
 def test_pln_to_pln():
-    # TODO: implement
-    assert False
+    e = CurrencyExchanger(exchange_rates={('eur', 'pln'): 4.24})
+    assert e.exchange(1, 'pln', 'pln') == 1
 
 
 # TODO: write your own test cases
@@ -48,10 +65,10 @@ def test_pln_to_pln():
 
 # uncomment for example test using mock
 # remember to import mock :)
-# @mock.patch('exchange.OnlineRates.get_data', mock.Mock(return_value={('eur', 'pln'): 4.24, }))
-# def test_eur_to_pln_with_data_from_external_api():
-#     online_rates = OnlineRates(src='https://currency-api.appspot.com/api/eur/pln.json').get_data()
-#     assert ('eur', 'pln') in online_rates
-#    e = CurrencyExchanger(exchange_rates=online_rates)
-#    assert e.exchange(1, 'eur', 'pln') == 4.24
-#    assert e.exchange(2, 'eur', 'pln') == 8.48
+@mock.patch('exchange.OnlineRates.get_data', mock.Mock(return_value={('eur', 'pln'): 4.24, }))
+def test_eur_to_pln_with_data_from_external_api():
+    online_rates = OnlineRates(src='https://currency-api.appspot.com/api/eur/pln.json').get_data()
+    assert ('eur', 'pln') in online_rates
+    e = CurrencyExchanger(exchange_rates=online_rates)
+    assert e.exchange(1, 'eur', 'pln') == 4.24
+    assert e.exchange(2, 'eur', 'pln') == 8.48
